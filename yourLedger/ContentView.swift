729e28fbb14cc16059389b084db020de89a87590
Sonal <sonal@magicbricks.com>
2025-02-14 16:54:26 +0530

    initial splash add

--- yourLedger/ContentView.swift
+++ yourLedger/ContentView.swift
@@ -8,34 +8,41 @@
 import SwiftUI
 import SwiftData
 
+enum OurAppState {
+    case launch
+    case foreground
+}
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    @State var path: [String] = []
+    @State var ourAppState: OurAppState = .launch
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        switch ourAppState {
+        case .launch:
+            SplashView(ourAppState: $ourAppState)
+        case .foreground:
+            NavigationStack(path: $path) {
+                TabView {
+                    Group {
+                        Text("a")
+                            .tabItem {
+                                Image(systemName: "book")
+                                Text("Expenses")
+                            }
+                            .tag(0)
+                        
+                        Text("b")
+                            .tabItem {
+                                Image(systemName: "indianrupeesign.circle")
+                                Text("Income")
+                            }
+                            .tag(1)
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
     }
 
@@ -55,7 +62,64 @@ struct ContentView: View {
     }
 }
 
+struct SplashView: View {
+    @State var iconShown = false
+    @Binding var ourAppState: OurAppState
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.black)
+                .ignoresSafeArea(.all)
+            
+            if iconShown {
+                Text("\u{20B9}")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color.white)
+                    .transition(.scale)
+            }
+        }
+        .onAppear {
+            withAnimation {
+                iconShown = true
+            } completion: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    ourAppState = .foreground
+                }
+            }
+        }
+    }
+}
+
 #Preview {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
+    
 }
+
+/*
+ NavigationSplitView {
+     List {
+         ForEach(items) { item in
+             NavigationLink {
+                 Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+             } label: {
+                 Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+             }
+         }
+         .onDelete(perform: deleteItems)
+     }
+     .toolbar {
+         ToolbarItem(placement: .navigationBarTrailing) {
+             EditButton()
+         }
+         ToolbarItem {
+             Button(action: addItem) {
+                 Label("Add Item", systemImage: "plus")
+             }
+         }
+     }
+ } detail: {
+     Text("Select an item")
+ }
+ */
