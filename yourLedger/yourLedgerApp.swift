2f4c7cead8e11b492e0d1cdd2ca0d1e0524b1646
imsonalbajaj <imsonalbajaj@gmail.com>
2025-06-06 15:17:44 +0530

    change

--- yourLedger/yourLedgerApp.swift
+++ yourLedger/yourLedgerApp.swift
@@ -21,18 +21,37 @@ extension EnvironmentValues {
     }
 }
 
+// MARK: - Custom Environment Key for Expense Context
+
+private struct ExpenseModelContextKey: EnvironmentKey {
+    static let defaultValue: ModelContext? = nil
+}
+
+extension EnvironmentValues {
+    var expenseModelContext: ModelContext? {
+        get { self[ExpenseModelContextKey.self] }
+        set { self[ExpenseModelContextKey.self] = newValue }
+    }
+}
+
 // MARK: - App Entry Point
 
 @main
 struct yourLedgerApp: App {
     let incomeContainer: ModelContainer
+//    let expenseContainer: ModelContainer
 
     init() {
         let incomeSchema = Schema([Income.self])
         let incomeConfig = ModelConfiguration(for: Income.self, isStoredInMemoryOnly: false)
+//        
+//        let expenseSchema = Schema([Expense.self])
+//        let expenseConfig = ModelConfiguration(for: Expense.self, isStoredInMemoryOnly: false)
+//        
         
         do {
             self.incomeContainer = try ModelContainer(for: incomeSchema, configurations: [incomeConfig])
+//            self.expenseContainer = try ModelContainer(for: expenseSchema, configurations: [expenseConfig])
         } catch {
             fatalError("Could not create incomeContainer: \(error)")
         }
@@ -44,5 +63,6 @@ struct yourLedgerApp: App {
             ContentView()
         }
         .environment(\.incomeModelContext, incomeContainer.mainContext)
+//        .environment(\.expenseModelContext, expenseContainer.mainContext)
     }
 }
